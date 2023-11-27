ec8d76d80e9533d3b04e8b5777478e3c243ec046
Birendra Oli <dev.birendraoli@gmail.com>
2023-11-27 21:27:26 +0545

    Learning update

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Test from "./test"
+
+function App() {
+  return (
+    <>
+    <Test />
+    <h1>Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/test.js
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h2>This is test text </h2>
+    )
+}
+
+export default Test;
